6e484e709b431cfa0384a93f30decce65d102fd9
Steven Ngo <stevenxngo@login-students.khoury.northeastern.edu>
2023-12-07 02:43:05 -0500

    jsons for user schema

--- users/schema.js
+++ users/schema.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import championsList from "./champions.json" assert { type: 'json' };
+
 const userSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
@@ -24,6 +26,52 @@ const userSchema = new mongoose.Schema(
         message: "Position required for players",
       },
     },
+    links: {
+      type: {
+        Twitter: {
+          type: String,
+          trim: true,
+          default: "",
+        },
+        Twitch: {
+          type: String,
+          trim: true,
+          default: "",
+        },
+        Youtube: {
+          type: String,
+          trim: true,
+          default: "",
+        },
+        Instagram: {
+          type: String,
+          trim: true,
+          default: "",
+        },
+        TikTok: {
+          type: String,
+          trim: true,
+          default: "",
+        },
+        Facebook: {
+          type: String,
+          trim: true,
+          default: "",
+        },
+      },
+      default: {},
+    },
+    champion: {
+      type: String,
+      enum: championsList,
+    },
+    summonerName: {
+      type: String
+    },
+    region: {
+      type: String,
+      enum: ["BR1", "EUN1", "EUW1", "JP1", "KR", "LA1", "LA2", "NA1", "OC1", "RU", "TR1"],
+    }
   },
   { collection: "users" }
 );
